9f3d64437745658474ad5c83ad4eb440a527fbfe
Jhon Lennon da Silva santos <jhonuot11@gmail.com>
2023-03-30 10:29:07 -0300

    adiconei o cabeçalho
    adiconei o cabeçalho de modo responsivo para que o itens de navegaçao fiquem ocultos e so sejam mostrados caso usuario clique no botao de hamburguer.

---
+++ mobile-navbar.js
@@ -0,0 +1,30 @@
+var botoes = document.querySelectorAll('button');
+  
+for (var i = 0; i < botoes.length; i++) {
+  botoes[i].addEventListener('click', function() {
+    var descricao = this.parentNode.querySelector('.descricao');
+    if (descricao.style.display === "none") {
+      descricao.style.display = "block";
+      this.textContent = "ocultar descricao";
+    } else {
+      descricao.style.display = "none";
+      this.textContent = "mostrar descricao";
+    }
+  });
+}
+
+
+const btn = document.getElementById("hamburguer");
+
+btn.addEventListener("click",mostrarMenu);
+
+
+function mostrarMenu(){
+    const navList = document.getElementById("nav-list");
+    if(navList.classList.contains("active")){
+        navList.classList.remove("active")
+    }else{
+        navList.classList.add("active")
+    };  
+}
+
